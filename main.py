f9f542f6d6cc976387bf92c485c8106864eee139
Elvis Sánchez <elvis.sanchez.napoles@gmail.com>
2020-08-07 02:12:10 -0500

    Add files

--- main.py
+++ main.py
@@ -1,78 +1,83 @@
+import enum
 import json
 import os
+import random
 import re
 import time
-from random import randint
 
 import vlc
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson import TextToSpeechV1
+from datetime import datetime
 
 
-def generate_speech(path, text, text_to_speech):
-    if not exists(path):
-        with open(file=path, mode='wb') as audio_file:
-            audio_file.write(
-                text_to_speech.synthesize(
-                    text=text,
-                    voice='en-US_AllisonV3Voice',
-                    accept='audio/wav'
-                ).get_result().content)
+class Hello(enum.Enum):
+    GoodMorning = 'good_morning'
+    GoodAfternoon = 'good_afternoon'
+    GoodEvening = 'good_evening'
+    Morning = 'morning'
 
 
-def exists(path):
-    return os.path.exists('./{}'.format(path))
+class Tense(enum.Enum):
+    Infinitive = 'infinitive'
+    PastSimple = 'past_simple'
+    PastParticiple = 'past_participle'
+
+
+def play(path):
+    if os.path.exists('./{}.wav'.format(path)):
+        vlc.MediaPlayer('./{}.wav'.format(path)).play()
+    else:
+        print('The path \'{}\' does not exist.'.format(path))
 
 
 def main():
-    text_to_speech_iam_apikey = os.getenv('TEXT_TO_SPEECH_IAM_APIKEY')
-    text_to_speech_url = os.getenv('TEXT_TO_SPEECH_URL')
-
-    authenticator = IAMAuthenticator(text_to_speech_iam_apikey)
-    text_to_speech = TextToSpeechV1(
-        authenticator=authenticator
-    )
-
-    text_to_speech.set_service_url(text_to_speech_url)
+    now = datetime.now()
 
     with open(file='data.json', mode='r') as json_file:
         irregular_verbs = json.load(json_file)['irregular_verbs']
 
-    generate_speech(path='good.wav',
-                    text='good!',
-                    text_to_speech=text_to_speech)
+    if 23 > now.hour < 12:
+        if random.choice([True, False]):
+            play(Hello.GoodMorning.value)
+        else:
+            play(Hello.Morning.value)
+    elif 18 > now.hour > 11:
+        play(Hello.GoodAfternoon.value)
+    else:
+        play(Hello.GoodEvening.value)
 
-    generate_speech(path='try_again.wav',
-                    text='try again!',
-                    text_to_speech=text_to_speech)
-
-    number_of_inputs = len(irregular_verbs)
-    i = 0
-
-    for irregular_verb in irregular_verbs:
-        number_of_inputs = number_of_inputs
-        i += 1
-        print('generate {} of {} audios'.format(i, number_of_inputs))
-        irregular_verb, tenses = irregular_verb.values()
-        generate_speech(path='{}.{}'.format(irregular_verb, 'wav'),
-                        text='{}, {}'.format(irregular_verb, ', and '.join(tenses)),
-                        text_to_speech=text_to_speech)
-        generate_speech(path='{}_spell.{}'.format(irregular_verb, 'wav'),
-                        text=', '.join([letter for letter in re.split(r'(\w)', irregular_verb) if letter.isalpha()]),
-                        text_to_speech=text_to_speech)
+    time.sleep(1)
 
     while True:
-        irregular_verb = irregular_verbs[randint(0, len(irregular_verbs) - 1)]
+        irregular_verb = random.choice(irregular_verbs)
         irregular_verb, tenses = irregular_verb.values()
-        print('\n{}\n{}\n'.format(irregular_verb, ', and '.join(tenses)))
-        vlc.MediaPlayer('./{}.{}'.format(irregular_verb, 'wav')).play()
-        time.sleep(3)
-        vlc.MediaPlayer('./{}_spell.{}'.format(irregular_verb, 'wav')).play()
-        print(' '.join([letter for letter in re.split(r'(\w)', irregular_verb) if letter.isalpha()]))
-        while input('type \'{}\': '.format(irregular_verb)).lower() != irregular_verb:
-            vlc.MediaPlayer('./try_again.wav').play()
+
+        print(irregular_verb)
+        play(irregular_verb)
+        time.sleep(1)
+
+        for letter in re.split(r'(\w)', irregular_verb):
+            if letter.isalpha():
+                print(letter, end=' ')
+                play(letter)
+                time.sleep(1)
+
+        print('\n{}'.format(', and '.join(tenses)))
+
+        if 'infinitive' in tenses:
+            play(Tense.Infinitive.value)
             time.sleep(1)
-        vlc.MediaPlayer('./good.wav').play()
+        if 'past simple' in tenses:
+            play(Tense.PastSimple.value)
+            time.sleep(1)
+        if 'past participle' in tenses:
+            play(Tense.PastParticiple.value)
+            time.sleep(1)
+
+        while input('type \'{}\': '.format(irregular_verb)).lower() != irregular_verb:
+            play(random.choice(['try_again', 'try_it_one_more_time']))
+            time.sleep(1)
+
+        play(random.choice(['approved', 'go_ahead', 'nice', 'ok', 'okeydokey', 'very_well']))
         time.sleep(1)
 
 
