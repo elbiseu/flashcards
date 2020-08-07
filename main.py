f102011fbdc2322e642bc81b628f838a887a253e
Elvis Sánchez <elvis.sanchez.napoles@gmail.com>
2020-08-07 02:43:29 -0500

    Delete enumerators

--- main.py
+++ main.py
@@ -9,22 +9,10 @@ from datetime import datetime
 import vlc
 
 
-class Hello(enum.Enum):
-    GoodMorning = 'good_morning'
-    GoodAfternoon = 'good_afternoon'
-    GoodEvening = 'good_evening'
-    Morning = 'morning'
-
-
-class Tense(enum.Enum):
-    Infinitive = 'infinitive'
-    PastSimple = 'past_simple'
-    PastParticiple = 'past_participle'
-
-
 def play(path):
     if os.path.exists('./resources/{}.wav'.format(path)):
         vlc.MediaPlayer('./resources/{}.wav'.format(path)).play()
+        time.sleep(1)
     else:
         print('The path \'{}\' does not exist.'.format(path))
 
@@ -32,20 +20,15 @@ def play(path):
 def main():
     now = datetime.now()
 
-    with open(file='data.json', mode='r') as json_file:
+    with open('data.json', 'r') as json_file:
         irregular_verbs = json.load(json_file)['irregular_verbs']
 
     if 23 > now.hour < 12:
-        if random.choice([True, False]):
-            play(Hello.GoodMorning.value)
-        else:
-            play(Hello.Morning.value)
+        play(random.choice(['good_morning', 'morning']))
     elif 18 > now.hour > 11:
-        play(Hello.GoodAfternoon.value)
+        play('good_afternoon')
     else:
-        play(Hello.GoodEvening.value)
-
-    time.sleep(1)
+        play('good_evening')
 
     while True:
         irregular_verb = random.choice(irregular_verbs)
@@ -53,29 +36,18 @@ def main():
 
         print(irregular_verb)
         play(irregular_verb)
-        time.sleep(1)
 
         for letter in re.split(r'(\w)', irregular_verb):
             if letter.isalpha():
                 play(letter)
-                time.sleep(1)
 
-        if 'infinitive' in tenses:
-            play(Tense.Infinitive.value)
-            time.sleep(1)
-        if 'past simple' in tenses:
-            play(Tense.PastSimple.value)
-            time.sleep(1)
-        if 'past participle' in tenses:
-            play(Tense.PastParticiple.value)
-            time.sleep(1)
+        for tense in tenses:
+            play(tense)
 
         while input('type \'{}\': '.format(irregular_verb)).lower() != irregular_verb:
             play(random.choice(['try_again', 'try_it_one_more_time']))
-            time.sleep(1)
 
         play(random.choice(['approved', 'go_ahead', 'nice', 'ok', 'okeydokey', 'very_well']))
-        time.sleep(1)
 
 
 if __name__ == '__main__':
