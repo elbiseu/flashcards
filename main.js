0b6a343c3d98b5f95d0ca03e67bf51d1e5408d5b
Elvis Sánchez <elvis.sanchez.napoles@gmail.com>
2020-11-15 01:21:07 -0600

    Add random numbers

--- main.js
+++ main.js
@@ -1,58 +1,55 @@
-let listOfIrregularVerbs = [
-  {
-    id: 1,
-    infinitive: "be",
-    past_simple: "was/were",
-    past_participle: "been"
-  },
-  {
-    id: 2,
-    infinitive: "beat",
-    past_simple: "beat",
-    past_participle: "beaten"
-  },
-  {
-    id: 3,
-    infinitive: "become",
-    past_simple: "became",
-    past_participle: "become"
-  },
-  {
-    id: 4,
-    infinitive: "begin",
-    past_simple: "began",
-    past_participle: "begun"
-  },
+let infinitive = [
+  "be",
+  "beat"
 ];
 
-for (let irregularVerb of listOfIrregularVerbs) {
-  for (let key of Object.keys(irregularVerb)) {
-    if (key !== "id") {
-      const flashcard = document.createElement("div");
-      flashcard.id = irregularVerb["id"] + key;
-      flashcard.className = "flashcard";
-      flashcard.draggable = true;
-      flashcard.innerText = irregularVerb[key];
-      flashcard.onclick = function () {
-        const src = "./resources/" + irregularVerb[key] + ".wav";
-        const pronunciation = new Audio(src);
-        const promise = pronunciation.play();
-        if (promise !== null) {
-          promise.then(function () {
-            console.log("Automatic playback started!");
-          }).catch(function (error) {
-            console.log("Automatic playback failed!");
-            console.log(error);
-          });
-        }
-      };
-      flashcard.ondragstart = function (event) {
-        event.dataTransfer.setData("text/plain", this.id);
-      };
-      const element = document.getElementById("flashcard_container");
-      element.appendChild(flashcard);
-    }
+let pastSimple = [
+  "was/were",
+  "beat"
+];
+
+let pastParticiple = [
+  "been",
+  "beaten"
+];
+
+let irregularVerbs = infinitive.concat(pastSimple);
+irregularVerbs = irregularVerbs.concat(pastParticiple);
+let identifiers = [];
+
+while (identifiers.length !== irregularVerbs.length) {
+  const random = Math.random() * irregularVerbs.length;
+  const floor = Math.floor(random);
+  if (!identifiers.includes(floor)) {
+    identifiers.push(floor);
   }
+}
+
+for (let identifier of identifiers) {
+  let irregularVerb = irregularVerbs[identifier];
+  const flashcard = document.createElement("div");
+  flashcard.id = identifier;
+  flashcard.className = "flashcard";
+  flashcard.draggable = true;
+  flashcard.innerText = irregularVerb;
+  flashcard.onclick = function () {
+    const src = "./resources/" + irregularVerb + ".wav";
+    const pronunciation = new Audio(src);
+    const promise = pronunciation.play();
+    if (promise !== null) {
+      promise.then(function () {
+        console.log("Automatic playback started!");
+      }).catch(function (error) {
+        console.log("Automatic playback failed!");
+        console.log(error);
+      });
+    }
+  };
+  flashcard.ondragstart = function (event) {
+    event.dataTransfer.setData("text/plain", this.id);
+  };
+  const element = document.getElementById("flashcard_container");
+  element.appendChild(flashcard);
 }
 
 for (let classification of ["infinitive", "past_simple", "past_participle", "flashcard"]) {
