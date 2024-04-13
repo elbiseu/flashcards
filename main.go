208222e24b6c96b52ec80ec58dc9c4a480e25add
elbiseu <elvis.sanchez.napoles@gmail.com>
2024-04-12 19:28:04 -0600

    Add middleware

--- main.go
+++ main.go
@@ -2,19 +2,56 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/elbiseu/flashcards/arangodb"
+	"github.com/elbiseu/flashcards/interfaces"
 	"github.com/elbiseu/flashcards/structures"
 	"log"
 	"net/http"
 	"os"
 )
 
-func main() {
-	arangoDBUsername, _ := os.LookupEnv("ARANGODB_USERNAME")
-	arangoDBPassword, _ := os.LookupEnv("ARANGODB_PASSWORD")
-	arangoDBEndpoint, _ := os.LookupEnv("ARANGODB_ENDPOINT")
-	arangoDBName, _ := os.LookupEnv("ARANGODB_DB")
+var (
+	arangoDBUsername, _ = os.LookupEnv("ARANGODB_USERNAME")
+	arangoDBPassword, _ = os.LookupEnv("ARANGODB_PASSWORD")
+	arangoDBEndpoint, _ = os.LookupEnv("ARANGODB_ENDPOINT")
+	arangoDBName, _     = os.LookupEnv("ARANGODB_DB")
+	store               interfaces.Store
+)
+
+type CustomRes struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func sendResponse(w http.ResponseWriter, status int, message string, data interface{}) {
+	res := CustomRes{
+		Status:  status,
+		Message: message,
+		Data:    data,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	// TODO.
+	_ = json.NewEncoder(w).Encode(res)
+}
+
+func middleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Recovered from panic: %v", err)
+				http.Error(w, "", http.StatusInternalServerError)
+			}
+		}()
+		next(w, r)
+	}
+}
+
+func init() {
 	arangoDB, err := arangodb.NewArangoDB(arangoDBUsername, arangoDBPassword, []string{arangoDBEndpoint})
 	if err != nil {
 		return
@@ -24,33 +61,43 @@ func main() {
 	if err != nil {
 		return
 	}
-	v := structures.Flashcard{
-		Value: "",
-		Type:  structures.IrregularVerb,
-		Meta: structures.Meta{
-			BaseForm:       "",
-			PastSimple:     "",
-			PastParticiple: "",
-		},
-	}
-	if err := arangodb.NewOperation(database).Save(ctx, v); err != nil {
-		log.Println(err)
-		return
-	}
+	var db interfaces.Store = arangodb.NewOperation(database)
+	store = db
+}
+
+func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
-	serveMux.HandleFunc("/flashcard/:type", func(responseWriter http.ResponseWriter, request *http.Request) {
-		v := structures.Flashcard{
-			Value: "",
-			Type:  structures.IrregularVerb,
-			Meta: structures.Meta{
-				BaseForm:       "",
-				PastSimple:     "",
-				PastParticiple: "",
-			},
+	serveMux.HandleFunc("/flashcard", middleware(func(responseWriter http.ResponseWriter, request *http.Request) {
+		switch request.Method {
+		case http.MethodPost:
+			v := structures.Flashcard{
+				Value: "",
+				Type:  structures.IrregularVerb,
+				Meta: structures.Meta{
+					BaseForm:       "",
+					PastSimple:     "",
+					PastParticiple: "",
+				},
+			}
+			if err := store.Save(request.Context(), v); err != nil {
+				log.Println(err)
+				return
+			}
 		}
-		fmt.Println(v)
-	})
+	}))
+	serveMux.HandleFunc("/flashcard/{key}", middleware(func(responseWriter http.ResponseWriter, request *http.Request) {
+		switch request.Method {
+		case http.MethodGet:
+			key := request.PathValue("key")
+			var v structures.Flashcard
+			if err := store.Get(request.Context(), key, v); err != nil {
+				log.Println(err)
+				return
+			}
+			sendResponse(responseWriter, http.StatusOK, "OK", v)
+		}
+	}))
 	for e := structures.List.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)
 	}
